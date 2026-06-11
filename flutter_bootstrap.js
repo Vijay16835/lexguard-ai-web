570ced19f778cd65bbdc02e3bf516fabcfd6ad6e
Vijay16835 <tvijay1098@gmail.com>
2026-06-11 07:40:20 +0000

    Deploying to gh-pages from @ Vijay16835/lexguard-ai-web@376d24efb4eff047e3681302187cf9d980859e9e 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "532777188" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "736138105" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
